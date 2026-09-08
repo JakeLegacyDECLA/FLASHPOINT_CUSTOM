c582d0f7d149cb6096c6011fb84136cf11c9dad8
Valentino Ortiz <A01712296@tec.mx>
2026-09-08 17:01:08 -0600

    feat: environment now as background

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+/// <summary>
+/// This Follow Player class will make the camera follow the player.
+/// Standar coding documentation can be found in 
+/// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
+/// </summary>
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0,6,-7);
+
+    /// <summary>
+    /// This method is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// This method is called once per frame
+    /// </summary>
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
